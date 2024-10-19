144171a4a352b7469dc46060170caf7544a24f7e
aluno <aluno@ifpe>
2024-10-19 09:39:43 -0300

    configuração inicial do projeto

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiEllenApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiEllenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiEllenApplication.class, args);
+	}
+
+}
